7d2fb038ff3bfbe380903b663ee369757c0e9789
Yusuf <yusuf.bestas@orgino.com.tr>
2019-01-14 02:46:55 +0300

    images add

--- javascript/script.js
+++ javascript/script.js
@@ -60,3 +60,20 @@ settings = {
     $(settings.objModalCloseBtn).bind("click", function () {
         $(".modal").fadeOut();
     });
+
+    // prograserr bar
+
+
+var forEach = function (array, callback, scope) {
+	for (var i = 0; i < array.length; i++) {
+		callback.call(scope, i, array[i]);
+	}
+};
+window.onload = function(){
+	var max = -219.99078369140625;
+	forEach(document.querySelectorAll('.progress'), function (index, value) {
+	percent = value.getAttribute('data-progress');
+		value.querySelector('.fill').setAttribute('style', 'stroke-dashoffset: ' + ((100 - percent) / 100) * max);
+		value.querySelector('.value').innerHTML = percent + '%';
+	});
+}
