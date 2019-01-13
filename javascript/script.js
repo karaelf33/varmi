e45ab855f6eeb55ba348c0a695d089fc89d0605c
Yusuf <yusuf.bestas@orgino.com.tr>
2019-01-13 13:59:18 +0300

    slinen dosya geri getirildi

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,57 @@ function openSearch() {
 function closeSearch() {
   document.getElementById("myOverlay").style.display = "none";
 }
+// favori star
+$('.click').click(function() {
+	if ($('span').hasClass("fa-star")) {
+			$('.click').removeClass('active')
+		setTimeout(function() {
+			$('.click').removeClass('active-2')
+		}, 30)
+			$('.click').removeClass('active-3')
+		setTimeout(function() {
+			$('span').removeClass('fa-star')
+			$('span').addClass('fa-star-o')
+		}, 15)
+	} else {
+		$('.click').addClass('active')
+		$('.click').addClass('active-2')
+		setTimeout(function() {
+			$('span').addClass('fa-star')
+			$('span').removeClass('fa-star-o')
+		}, 150)
+		setTimeout(function() {
+			$('.click').addClass('active-3')
+		}, 150)
+		$('.info').addClass('info-tog')
+		setTimeout(function(){
+			$('.info').removeClass('info-tog')
+		},1000)
+	}
+})
+// popup
+
+
+
+settings = {
+    //Model Popup
+    objModalPopupBtn: ".modalButton",
+    objModalCloseBtn: ".overlay, .closeBtn",
+    objModalDataAttr: "data-popup"
+}
+  $(settings.objModalPopupBtn).bind("click", function () {
+        if ($(this).attr(settings.objModalDataAttr)) {
+
+            var strDataPopupName = $(this).attr(settings.objModalDataAttr);
+
+
+            //Fade In Modal Pop Up
+            $(".overlay, #" + strDataPopupName).fadeIn();
+
+        }
+    });
+
+    //On clicking the modal background
+    $(settings.objModalCloseBtn).bind("click", function () {
+        $(".modal").fadeOut();
+    });
